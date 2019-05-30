0e65e03cf43d05d4c1cc0e41a759a2f0f2aa4da6
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 21:04:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,7 @@
 #AlyssaYesilyurt
 
-def badge_maker(name)
-  puts "Hello my name is #{name}."
-end 
-
 def batch_badge_creator(array_names)
   array_names.each do |name|
-    puts badge_maker(array_names)
+    puts "Hello , my name is #{name}."
   end
 end
