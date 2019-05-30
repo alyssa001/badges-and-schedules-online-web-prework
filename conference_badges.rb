bdc4e1ac5f2cd5c6207c358b359fc226c1a34d1d
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 21:09:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,12 @@
 #AlyssaYesilyurt
 
 def badge_maker(name)
-  return "Hello my name is #{name}."
+  puts "Hello my name is #{name}."
 end 
 
 def batch_badge_creator(guests)
-  arr = [ ]
-  guests.each { |name| arr << badge_maker(name) }
-  return arr
+  array_names = [ ]
+  guests.each do |name|
+    puts badge_maker(name)
+  end
 end
